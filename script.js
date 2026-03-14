9515543e767ea671434f9d078d84c3c129b3466a
Kavipriya Chakkaravarthi <ckavipriya2006@gmail.com>
2026-03-14 22:03:46 +0530

    Remove showMessage function and update bin statuses

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-function showMessage() {
-    alert("Welcome to the Smart Waste Management System!");
+window.onload = function(){
+
+document.getElementById("bin1").innerHTML = "Full ⚠️";
+document.getElementById("bin2").innerHTML = "Half Filled";
+document.getElementById("bin3").innerHTML = "Empty";
+
 }
