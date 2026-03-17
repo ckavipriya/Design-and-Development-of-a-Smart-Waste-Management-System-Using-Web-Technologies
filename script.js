bed6d4a0cf212eb2b31d5a0f8e52fa22050dbf11
Kavipriya Chakkaravarthi <ckavipriya2006@gmail.com>
2026-03-18 04:43:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
-function openDashboard(){
+function openChat(){
+document.getElementById("chatbox").style.display="block";
+}
 
-window.location.href="status.html";
+function closeChat(){
+document.getElementById("chatbox").style.display="none";
+}
+
+function sendMessage(){
+
+let input=document.getElementById("userInput").value.toLowerCase();
+
+let chat=document.getElementById("chat-body");
+
+let response="";
+
+if(input.includes("bin")){
+response="Smart bins monitor waste levels using sensors.";
+}
+
+else if(input.includes("status")){
+response="You can check the waste level in the Status page dashboard.";
+}
+
+else if(input.includes("project")){
+response="This is a Smart Waste Management System that helps cities manage garbage efficiently.";
+}
+
+else if(input.includes("report")){
+response="Waste collection reports are available in the Report section.";
+}
+
+else{
+response="Sorry, I can answer questions about the waste system.";
+}
+
+chat.innerHTML += "<p><b>You:</b> "+input+"</p>";
+chat.innerHTML += "<p><b>AI:</b> "+response+"</p>";
+
+document.getElementById("userInput").value="";
 
 }
