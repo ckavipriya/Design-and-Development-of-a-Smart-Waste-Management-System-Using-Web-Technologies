d70980a61da01f1636c917b5b9b48ddca7e6c9aa
Kavipriya Chakkaravarthi <ckavipriya2006@gmail.com>
2026-03-17 16:05:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-window.onload = function(){
+function showNotification(){
 
-document.getElementById("bin1").innerHTML = "Full ⚠️";
-document.getElementById("bin2").innerHTML = "Half Filled";
-document.getElementById("bin3").innerHTML = "Empty";
+alert("⚠ Smart Alert: Garbage bin in Area A is almost full. Collection required!");
+
+}
+
+function aiReply(){
+
+let q = document.getElementById("question").value.toLowerCase();
+
+if(q.includes("plastic")){
+document.getElementById("answer").innerHTML =
+"Plastic should be placed in the recycling bin.";
+}
+
+else if(q.includes("food")){
+document.getElementById("answer").innerHTML =
+"Food waste should go into the organic waste bin.";
+}
+
+else if(q.includes("glass")){
+document.getElementById("answer").innerHTML =
+"Glass should be disposed in recyclable waste.";
+}
+
+else{
+document.getElementById("answer").innerHTML =
+"Please separate waste into recyclable, organic and non-recyclable.";
+}
 
 }
