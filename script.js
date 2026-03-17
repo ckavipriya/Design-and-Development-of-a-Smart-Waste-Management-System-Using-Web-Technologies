7e44482c1c9936bcb1fc5f79f88b9a121ec0c9f5
Kavipriya Chakkaravarthi <ckavipriya2006@gmail.com>
2026-03-17 22:01:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
-function showStatus(){
+function openDashboard(){
 
-alert("Smart Alert: Garbage bin in Area A is almost full.");
-
-}
-
-function aiReply(){
-
-let q=document.getElementById("question").value.toLowerCase();
-
-if(q.includes("plastic")){
-document.getElementById("answer").innerHTML="Plastic should go to recycling bin.";
-}
-
-else if(q.includes("food")){
-document.getElementById("answer").innerHTML="Food waste goes in organic bin.";
-}
-
-else{
-document.getElementById("answer").innerHTML="Separate waste into recyclable and non-recyclable.";
-}
+window.location.href="status.html";
 
 }
