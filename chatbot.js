e3ba849987fb4a1c5f53b461d3552209648b2caf
Kavipriya Chakkaravarthi <ckavipriya2006@gmail.com>
2026-03-23 05:30:28 +0530

    Update and rename ai-script.js to chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -11,20 +11,20 @@ function sendMsg(){
 let input=document.getElementById("chatInput").value
 let box=document.getElementById("chatMessages")
 
-box.innerHTML += "<p><b>You:</b> "+input+"</p>"
+box.innerHTML+="<p><b>You:</b> "+input+"</p>"
 
 let reply="I can help with waste management information."
 
 if(input.toLowerCase().includes("bin"))
-reply="Smart bins use sensors to monitor garbage levels."
+reply="Smart bins use sensors to detect garbage levels."
 
 else if(input.toLowerCase().includes("report"))
-reply="You can report waste issues in the report page."
+reply="Please go to the Report page to report waste."
 
 else if(input.toLowerCase().includes("contact"))
-reply="Please check the contact page for support."
+reply="You can find contact information in the Contact page."
 
-box.innerHTML += "<p><b>Bot:</b> "+reply+"</p>"
+box.innerHTML+="<p><b>Bot:</b> "+reply+"</p>"
 
 document.getElementById("chatInput").value=""
 }
